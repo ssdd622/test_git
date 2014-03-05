1219fae735945f27e9154c11566bbd8387416275
ssdd622 <fk1000@126.com>
2014-03-05 11:48:52 +0800

    mm3

--- src/tt1/Test_G.java
+++ src/tt1/Test_G.java
@@ -4,5 +4,6 @@ public class Test_G {
     public static void main(String[] args) {
         System.out.println("fffffff1");
         System.out.println("fffffff2");
+        System.out.println("fffffff3");
     }
 }
