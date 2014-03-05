8a92d186110d0a3135f8225c097ec7c058b24653
ssdd622 <fk1000@126.com>
2014-03-05 11:48:52 +0800

    kk10

--- src/tt1/Test_G.java
+++ src/tt1/Test_G.java
@@ -2,10 +2,5 @@ package tt1;
 
 public class Test_G {
     public static void main(String[] args) {
-        System.out.println("this is test");
-        System.out.println("this is in branch1");
-        System.out.println("why not push!!!");
-        System.out.println("chooise a branch dd1");
-        System.out.println("after merge");
     }
 }
