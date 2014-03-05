7e7daf44996d88eeb12e889d4caaae2d593830fb
ssdd622 <fk1000@126.com>
2014-03-05 09:35:24 +0800

    kk2

--- src/Test_G.java
+++ src/Test_G.java
@@ -5,5 +5,6 @@ public class Test_G {
     public static void main(String[] args) {
         System.out.println("this is test");
         System.out.println("this is in branch1");
+        System.out.println("why not push!!!");
     }
 }
